89642a01c50862f9e494871729530d6938e28199
Prasad Kavinda <pp.kavinda@gmail.com>
2023-10-31 10:23:19 +0530

    Update app.js

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,16 +1,12 @@
 import './bootstrap';
 import router from './routes';
-
 import User from './models/User';
 import NavBar from './components/NavBar';
-
 
 window.user = new User();
 
 const app = new Vue({
 	components: {NavBar},
-
-    el: '#app',
-    router,
-
+	el: '#app',
+	router,
 });
