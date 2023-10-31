f77d8f96c979e11a6e788593e832c02096c1442e
Prasad Kavinda <pp.kavinda@gmail.com>
2023-10-31 10:34:10 +0530

    Update Post.php

--- app/Post.php
+++ app/Post.php
@@ -11,7 +11,6 @@ class Post extends Model
 	];
     
     public function user () {
-    	return $this->belongsTo(User::class)
-    		->select(['id', 'name']);
+    	return $this->belongsTo(User::class)->select(['id', 'name']);
     }
 }
